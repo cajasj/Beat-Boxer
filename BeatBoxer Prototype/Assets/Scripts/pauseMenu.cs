0336f2a9a85b98e754d75684b8e9a7b46a5c0e65
cajasj <cajasjan@gmail.com>
2016-03-07 21:32:44 -0500

    Created Pause Menu

---
+++ BeatBoxer Prototype/Assets/Scripts/pauseMenu.cs
@@ -0,0 +1,56 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class pauseMenu : MonoBehaviour {
+    public bool isPaussed;
+    public GameObject pauseCanvas;
+    public GameObject panel;
+    public bool isClicked;
+	// Use this for initialization
+
+	
+	// Update is called once per frame
+	void Update () {
+	    if (isPaussed)
+        {
+            pauseCanvas.SetActive(true);
+            Time.timeScale = 0f;
+        }
+        else
+        {
+            pauseCanvas.SetActive(false);
+            Time.timeScale = 1f;
+            isClicked = false;
+        }
+        if (isClicked)
+        {
+            panel.SetActive(true);
+        }
+        else
+        {
+            panel.SetActive(false);
+        }
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            isPaussed = !isPaussed;
+            
+            
+        }
+    }
+    public void resume()
+    {
+        isPaussed = false;
+    }
+    public void openQuitSub()
+    {
+        isClicked = true;
+    }
+   public void noQuit()
+    {
+        isClicked=false;
+    }
+    public void exitGame()
+    {
+        Application.Quit();
+    }
+}
---
+++ BeatBoxer Prototype/Assets/Scripts/quitSubMenu.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class quitSubMenu : MonoBehaviour {
+    public bool quitClick;
+    public GameObject quitCanvas;
+    // Use this for initialization
+    void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (quitClick)
+        {
+            quitCanvas.SetActive(true);
+          
+        }
+        else
+        {
+            quitCanvas.SetActive(false);
+        }
+
+        
+    }
+    public void noQuit(bool isClick)
+    {
+        quitClick = !isClick;
+        Debug.Log("no quit function-");
+    }
+    public void exitGame()
+    {
+        Application.Quit();
+    }
+}

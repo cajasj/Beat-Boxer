349419278d22e8a37d6f4c3a330f5a6d23c25d5d
cajasj <cajasjan@gmail.com>
2016-03-13 01:33:23 -0500

    combo animation works

--- BeatBoxer Prototype/Assets/Scripts/uppercutAttack.cs
+++ BeatBoxer Prototype/Assets/Scripts/uppercutAttack.cs
@@ -3,14 +3,21 @@ using System.Collections;
 
 public class uppercutAttack : MonoBehaviour {
     // Use this for initialization
-    public float timeInBetweeen = .5f;
+    public float timeInBetweeen = .8f;
     int i;
-    
+    private float attackCoolDown = .3f;
+    private float attackTimer;
+    private Animator anim;
     private string[] keyWumboCombo = { "J", "J", "J", "I", "Space" };
     public KeyCode[] wumboCombo;
-    public Collider2D hookTrigger;
-    private Animator anim;
-   void Start()
+    private float comboReset;
+    private bool completeWumboCombo=false;
+    private bool upperCut=false;
+    void Awake()
+    {
+        anim = gameObject.GetComponent<Animator>();
+    }
+    void Start()
     {
         for(int k=0; k < keyWumboCombo.Length; k++)
         {
@@ -23,7 +30,23 @@ public class uppercutAttack : MonoBehaviour {
     }
     // Update is called once per frame
     void Update() {
+        resetTimer();
         detectButton();
+        if (completeWumboCombo && !upperCut)
+        {
+            upperCut = true;
+            attackTimer = attackCoolDown;
+        }
+        if (upperCut)
+        {
+            
+            animationCoolDown();
+            Debug.Log("out the cut " + upperCut);
+            anim.SetBool("uppercut", upperCut);
+            completeWumboCombo = false;
+        }
+        
+
     }
     void detectButton()
     {
@@ -31,16 +54,62 @@ public class uppercutAttack : MonoBehaviour {
         {
             if (Input.GetKeyDown(mahKeys))
             {
-                if (i < wumboCombo.Length&& Input.GetKeyDown(mahKeys) == Input.GetKeyDown(wumboCombo[i])) { 
-                Debug.Log("key pressed "+ mahKeys+" counter "+i);
-                i++;
+
+                if (i < wumboCombo.Length&& Input.GetKeyDown(mahKeys) == Input.GetKeyDown(wumboCombo[i])) {
+
+                    //Debug.Log("key pressed "+ mahKeys+" counter "+i);
+                    i++;
+                    comboReset = .8f;
+
+                }else
+                {
+                    i = 0;
+                    Debug.Log("messed up the combo you pressed " + mahKeys);
+
                 }
+                if (i >= wumboCombo.Length)
+                {
+                    Debug.Log("complete sequence of button press");
+                    completeWumboCombo = true;
+                    i = 0;
+                }
+               
             }
          
         }
 
-
     }
- }
+    void resetTimer()
+    {
+        if (comboReset>0)
+        {
+            comboReset -= Time.deltaTime;
+        }
+        else
+        {
+            i = 0;
+        }
+    }
+    void onlyAttack()
+    {
+        upperCut = true;
+        attackTimer = attackCoolDown;
+    }
+    void animationCoolDown()
+    {
+        if (attackTimer > 0)
+        {
+
+            attackTimer -= Time.deltaTime;
+        }
+        else
+        {
+            
+            upperCut = false;
+        }
+    }
+    
+
+}
    
 

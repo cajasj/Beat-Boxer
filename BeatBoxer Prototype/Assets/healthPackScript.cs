3eb889151cff8c7d96d3e528a4ee548f83b9dbc5
cajasj <cajasjan@gmail.com>
2016-03-29 06:45:42 -0400

    fixed UI due to Unity being trash in UI

---
+++ BeatBoxer Prototype/Assets/healthPackScript.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class healthPackScript : MonoBehaviour {
+    public BeatBoxerScript beatBoxerHealth;
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+    void OnTriggerEnter2D(Collider2D collide)
+    {
+        if (collide.name == "Player")
+        {
+            beatBoxerHealth.currentHealth = beatBoxerHealth.maxHealth;
+            beatBoxerHealth.guts = beatBoxerHealth.maxGuts;
+            Destroy(gameObject);
+        }
+    }
+}
